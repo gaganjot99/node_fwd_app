921365974bfb25ffcb3e6e685e8557aa4e2f1e81
Gagan Kahal <73100470+gaganjot99@users.noreply.github.com>
2022-12-24 20:47:17 +0530

    add: another logc

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const express = require('express');
+const request = require('request');
+
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
-app.use('*', createProxyMiddleware({target: "http://google.com", changeOrigin: true}));
-app.listen(3000);
+
+
+//app.use('*', createProxyMiddleware({target: "http://google.com", changeOrigin: true}));
+
+
+let apiServer = 'https://freecodecamp.com'
+app.use( function(req, res) {
+  console.log(req.originalUrl)
+  var url = apiServer + req.originalUrl
+  req.pipe(request(url)).pipe(res)
+});
+
+app.listen(3000, ()=>{
+  console.log('server started at 3000')
+})
